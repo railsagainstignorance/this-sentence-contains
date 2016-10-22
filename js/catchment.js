c4a685fe2026a2aa9e8f0a2276c6e3fbddf61544
Chris G <cgathercole@gmail.com>
2016-10-22 10:04:08 +0100

    now translating from ruby to JS - basic arrays

--- js/catchment.js
+++ js/catchment.js
@@ -1,8 +1,15 @@
 var Catchment = (function() {
-  var contents = 'contents'
+  INITIAL_RANGE = 30; // for generating the initial count of each letter
+
+  UNITS  = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  TENS   = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  POWERS = ['zero', 'ten', 'hundred', 'thousand'];
+
+  KNOWN_INTS = {}; // for memoizing all the numbers we come across
 
   var scan = function() {
       console.log("scanning now...");
+      console.log("UNITS=" + UNITS);
   }
 
   return {
