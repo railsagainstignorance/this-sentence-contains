017abc05bf93b349827a1075560749aeb66f31c0
Chris G <cgathercole@gmail.com>
2016-10-22 10:33:30 +0100

    a working int_to_words

--- js/catchment.js
+++ js/catchment.js
@@ -7,9 +7,45 @@ var Catchment = (function() {
 
   KNOWN_INTS = {}; // for memoizing all the numbers we come across
 
+  // e.g. 102 -> "one hundred and two"
+  var int_to_words = function( i ) {
+    if (! Number.isInteger(i)) {
+      return null;
+    } else if (i in KNOWN_INTS) {
+      return KNOWN_INTS[i];
+    };
+
+    var phrase;
+    if (i < 0) {
+      phrase = 'minus ' + int_to_words(-i);
+    } else if(i < 20) {
+      phrase = UNITS[i];
+    } else if(i < 100) {
+      var d = Math.floor(i / 10);
+      var m = i % 10;
+      phrase = TENS[d] + ((m>0)? ' ' + int_to_words(m) : '');
+    } else if(i < 1000) {
+      var d = Math.floor(i / 100);
+      var m = i % 100;
+      phrase = int_to_words(d) + ' ' + POWERS[2] + ((m>0)? ' and ' + int_to_words(m) : '');
+    } else if(i < 10000) {
+      var d = Math.floor(i / 1000);
+      var m = i % 1000;
+      phrase = int_to_words(d) + ' ' + POWERS[3] + ((m>0)? ' ' + int_to_words(m) : '');
+    } else {
+      phrase = 'more than 9999';
+    }
+    
+    KNOWN_INTS[i] = phrase;
+    return phrase;
+  }
+
+
+
   var scan = function() {
       console.log("scanning now...");
       console.log("UNITS=" + UNITS);
+      console.log("int_to_words(4176)=" + int_to_words(4176));
   }
 
   return {
