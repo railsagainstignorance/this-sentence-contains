677c9c9be60e18c277c5e1a1aa096dbc03aab58a
Chris G <cgathercole@gmail.com>
2016-10-22 09:53:27 +0100

    toying with a basic JS impl to display the multi-dim catchment area

---
+++ js/catchment.js
@@ -0,0 +1,14 @@
+var Catchment = (function() {
+  var contents = 'contents'
+
+  var scan = function() {
+      console.log("scanning now...");
+  }
+
+  return {
+    scan: function() {
+      scan();
+    }
+  };
+
+})();
